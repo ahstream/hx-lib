3895478374fd1a26da6ba57099876e6f045d453f
ahstream <hstream@gmail.com>
2023-10-25 22:54:06 +0200

    Add date constants

--- src/js/date.js
+++ src/js/date.js
@@ -7,6 +7,9 @@ export const ONE_SECOND = 1 * 1000;
 export const ONE_MINUTE = 60 * ONE_SECOND;
 export const ONE_HOUR = 60 * ONE_MINUTE;
 export const ONE_DAY = 24 * ONE_HOUR;
+export const ONE_WEEK = 7 * ONE_DAY;
+export const ONE_MONTH = 30 * ONE_DAY;
+export const ONE_YEAR = 365 * ONE_DAY;
 
 export function isValidDate(date) {
   // An invalid date object returns NaN for getTime() and NaN is the only object not strictly equal to itself.
